fc8702f5a51b72bba2bd11515df73a718fe67b91
ndb796 <ndb796@naver.com>
2019-05-30 17:08:21 +0900

    Add S3 Integration API

---
+++ s3/handler.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const manageFile = require('./manageFile');
+
+module.exports.appendText = (event, context, callback) => {
+  const text = event.queryStringParameters.text;
+  manageFile.appendText(text).then(result => {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        result
+      })
+    };
+    callback(null, response);
+  });
+};
